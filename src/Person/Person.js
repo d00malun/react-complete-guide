518543a7d1a7a3ee76a80787b948360b31089ada
ubyc <magnus.lundvall@skatteverket.se>
2019-02-03 14:13:24 +0100

    Removed random error

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,10 +2,6 @@ import React from 'react';
 import classes from './Person.module.css';
 
 const person = (props) => {
-  const random = Math.random();
-  if(random > 0.7){
-    throw new Error('Something went wrong!');
-  }
   return (
     <div className={classes.Person}>
       <p onClick={props.click}>I'm {props.name} and I am {props.age} year old!</p>
