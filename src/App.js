f24a058ed3dc1cf7512aff7775222d12a33f946e
ubyc <magnus.lundvall@skatteverket.se>
2019-02-05 22:26:06 +0100

    Removed ErrorBoundary

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 import classes from './App.module.css';
 import Person from './Person/Person';
-import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
 
 
 class App extends Component {
@@ -55,13 +54,13 @@ class App extends Component {
         <div>
           {
             this.state.persons.map((person, index) => {
-              return <ErrorBoundary key={person.id}>
-                <Person
+              return <Person
                   click={() => this.deletePersonHandler(index)}
                   name={person.name}
                   age={person.age}
+                  key={person.id}
                   changed={(event) => this.nameChangedHandler(event, person.id)}
-                /></ErrorBoundary>
+                />
             })
           }
         </div>
