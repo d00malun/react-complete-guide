46a26179b1cb103c7092d9d7978ae1a8b4a78ea2
Magnus Lundvall <lundvall@gmail.com>
2019-02-09 00:03:48 +0100

    withClass

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,7 +2,8 @@ import React, {Component} from 'react';
 import classes from './App.module.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit'
-import WithClass from '../hoc/WithClass';
+import withClass from '../hoc/withClass';
+import Auxiliary from '../hoc/Auxiliary';
 
 class App extends Component {
 
@@ -88,7 +89,7 @@ class App extends Component {
     }
 
     return (
-      <WithClass classes={classes.App}>
+      <Auxiliary>
         <button onClick={() => {
           this.setState({showCockpit: false})
         }}>Remove Cockpit
@@ -100,11 +101,11 @@ class App extends Component {
           clicked={this.togglePersonsHandler}
         /> : null}
         {persons}
-      </WithClass>
+      </Auxiliary>
     );
     // return React.createElement('div', {className:'App'}, React.createElement('h1', null, 'Does this work now??'));
   }
 
 }
 
-export default App;
+export default withClass(App, classes.App);
